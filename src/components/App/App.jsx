74337cdad3ffd6fc5fe96e6b5f6dae62b8197291
RuslanMirasov <info@mirasov.dev>
2023-09-17 12:58:27 +0200

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,24 @@
 import { Component } from 'react';
 import Searchbar from 'components/Searchbar/Searchbar';
+import Popup from 'components/Popup/Popup';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import { Button } from 'components/Button/Button';
 import { Error } from 'components/Error/Error';
-import { Oval } from 'react-loader-spinner';
+import { Loader } from 'components/Loader/Loader';
 import {fetcImages} from "../../api/api";
 import css from './App.module.css';
-import Popup from 'components/Popup/Popup';
+
 
 export class App extends Component{
 
   state = {
-    error: [],
     images: [],
     imagesTotal: 0,
     page: 1,
     modalImage: '',
     request: '',
+    error: '',
     isLoading: false,
     showModal: false,
   };
@@ -28,16 +29,18 @@ export class App extends Component{
     if (prevState.request !== request || prevState.page !== page) {
       this.setState({
         isLoading: true,
-        error: [],
+        error: '',
       });
       try {
         const newImages = await fetcImages(page, request);
-        newImages.totalHits > 0 ?
-          this.setState({ imagesTotal: newImages.totalHits }) :
-          this.setState({ error: ['Sorry','There are no images matching your search query. Please try again'] });
-        page === 1 ?
-          this.setState({ images: newImages.hits }) :
-          this.setState({ images: [...prevState.images, ...newImages.hits] });
+        if (newImages.totalHits === 0) {
+          this.setState({ error: ['Sorry', 'There are no images matching your search query. Please try again'] });
+          return;
+        }
+        this.setState({
+          images: [...this.state.images, ...newImages.hits],
+          imagesTotal: newImages.totalHits,
+        });
       } catch (error) {
         this.setState({ error: [error.message, error.request.responseText]});
       } finally {
@@ -51,8 +54,12 @@ export class App extends Component{
   };
 
   getFormRequest = newRequest => {
-    this.setState({request: newRequest, page: 1});
-    this.state.request !== newRequest && this.setState({images:[]});
+    this.state.request === newRequest ? alert(`Images for the query "${newRequest}" are already displayed`):
+    this.setState({
+      request: newRequest,
+      page: 1,
+      images:[],
+    });
   };
 
   getFormError = formError => {
@@ -74,23 +81,13 @@ export class App extends Component{
       <main className={css.App}>
         <Searchbar onSubmit={this.getFormRequest} onError={this.getFormError} />
         {error.length > 0 && <Error errorText={error} />}
-        {(images.length > 0 && error.length <= 0) && 
+        {(images.length > 0 && error.length === 0) && 
           <ImageGallery>
             <ImageGalleryItem images={images} shouPopup={this.onImageClick} />
           </ImageGallery>
         }
-        {(!isLoading && error.length <= 0 && images.length < imagesTotal) && <Button loadMore={this.onLoadMore} />}
-        <Oval
-          height={60}
-          width={60}
-          color="#5c91b1"
-          wrapperStyle={{ marginTop: '20px', }}
-          visible={isLoading}
-          ariaLabel='oval-loading'
-          secondaryColor="#d0d7dd"
-          strokeWidth={8}
-          strokeWidthSecondary={8}
-        />
+        {(!isLoading && error.length === 0 && images.length < imagesTotal) && <Button loadMore={this.onLoadMore} />}
+        <Loader showLoader={isLoading} />
         {showModal && <Popup url={modalImage} onClose={this.closeModal} />}
       </main>
     );
